e3f03a7f85371171fa359175f6517afdc6c6289b
Kaden Hardesty <82276766+TechnoWizzy@users.noreply.github.com>
2023-09-03 16:12:26 -0400

    Update game.embed.ts

--- src/embeds/game.embed.ts
+++ src/embeds/game.embed.ts
@@ -3,11 +3,18 @@ import {Game} from "../saveables/game";
 
 export class GameEmbed extends EmbedBuilder {
 
-    constructor() {
+    constructor(game: Game, message: string) {
         super();
+        this.setTitle(message);
     }
 
     public static async load(game: Game) {
-        return new GameEmbed();
+        if (game.teamOnePoints > game.teamTwoPoints) {
+            const message = `Team ${game.teamOneName} beat ${game.teamTwoName} ${game.teamOnePoints} - ${game.teamTwoPoints}`;
+            return new GameEmbed(game, message);
+        } else {
+            const message = `Team ${game.teamTwoName} beat ${game.teamOneName} ${game.teamTwoPoints} - ${game.teamOnePoints}`;
+            return new GameEmbed(game, message);
+        }
     }
 }
